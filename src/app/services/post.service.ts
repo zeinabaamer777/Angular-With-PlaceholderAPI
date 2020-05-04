c3a7112b1b5282f560bcfd4c3ba34d731380ac38
zeinabaamer777 <zeinabaamer157@gmail.com>
2020-05-04 13:46:52 +0200

    uploading ...

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+	constructor(private HttpClient:HttpClient) { }
+
+	fetchPosts(){
+	  	return this.HttpClient.get("https://jsonplaceholder.typicode.com/posts");
+	}
+
+	fetchPostById(id: number){
+	  	return this.HttpClient.get("https://jsonplaceholder.typicode.com/posts/"+ id);
+	}
+
+}
